d199b36d8e4792ab0b6d0dcb7b5e64a403c10c18
Phumelele <princessdlamini69@gmail.com>
2025-11-15 00:36:51 +0200

    Dynamic to-do list

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     //select DOM elements
-    const addButton = document.getElementById('add-task');
+    const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
